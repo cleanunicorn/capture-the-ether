464351be940366fa0ee8c46be743029ad83835a2
Daniel Luca <lucadanielcostin@gmail.com>
2018-10-10 22:38:13 +0300

    Add script to bruteforce number

---
+++ guess-the-secret-number/index.js
@@ -0,0 +1,9 @@
+web3 = require("web3");
+
+for (let i = 0; i < 100000000; i++) {
+    // 0xdb81b4d58595fbbbb592d3661a34cdca14d7ab379441400cbfa1b78bc447c365
+    if (web3.utils.sha3(`${i}`) == "0xdb81b4d58595fbbbb592d3661a34cdca14d7ab379441400cbfa1b78bc447c365") {
+        console.log(`Found = ${i}`);
+        break;
+    }
+}
