c5781d1df731d57cc70a3e7f6578097b206551e3
Daniel Luca <lucadanielcostin@gmail.com>
2018-10-11 12:11:02 +0300

    Test something that did not work

--- guess-the-secret-number/index.js
+++ guess-the-secret-number/index.js
@@ -1,8 +1,8 @@
 web3 = require("web3");
 
-for (let i = 0; i < 100000000; i++) {
+for (let i = 0; i < 255; i++) {
     // 0xdb81b4d58595fbbbb592d3661a34cdca14d7ab379441400cbfa1b78bc447c365
-    if (web3.utils.sha3(`${i}`) == "0xdb81b4d58595fbbbb592d3661a34cdca14d7ab379441400cbfa1b78bc447c365") {
+    if (web3.utils.soliditySha3('' + i) == "0xdb81b4d58595fbbbb592d3661a34cdca14d7ab379441400cbfa1b78bc447c365") {
         console.log(`Found = ${i}`);
         break;
     }
