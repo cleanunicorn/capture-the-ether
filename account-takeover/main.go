18f4d600e0bc9886a8dd521f77fb71bb0afbe7e4
Daniel Luca <lucadanielcostin@gmail.com>
2018-11-04 16:11:24 +0100

    Add bad rng

--- account-takeover/main.go
+++ account-takeover/main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+	"bytes"
+	"crypto/ecdsa"
 	"encoding/hex"
 	"fmt"
-
-	"crypto/ecdsa"
-	"crypto/rand"
+	"io"
+	"math/big"
 
 	"github.com/ethereum/go-ethereum/crypto"
 )
+
+type BadRand struct {
+	reader             io.Reader
+	expectedTotalBytes uint
+}
+
+func NewBadRand(out int64, outputLen uint) *BadRand {
+	x := big.NewInt(out)
+	y := make([]byte, outputLen)
+	tail := x.Bytes()
+
+	for i, j := len(tail)-1, len(y)-1; i >= 0 && j > 0; i, j = i-1, j-1 {
+		y[j] = tail[i]
+	}
+
+	return &BadRand{
+		reader:             bytes.NewReader(y),
+		expectedTotalBytes: outputLen,
+	}
+}
+
+func (b *BadRand) Read(p []byte) (n int, err error) {
+	return b.reader.Read(p)
+}
 
 // Address returns the address as a string
 func Address(key *ecdsa.PrivateKey) string {
@@ -21,8 +46,14 @@ func PrivateKey(key *ecdsa.PrivateKey) string {
 }
 
 func main() {
-	for {
-		k, _ := ecdsa.GenerateKey(crypto.S256(), rand.Reader)
+	var i int64
+	for i = 0; true; i++ {
+		br := NewBadRand(i, 40)
+
+		k, err := ecdsa.GenerateKey(crypto.S256(), br)
+		if err != nil {
+			fmt.Println("Generate key error: ", err)
+		}
 
 		pubBytes := crypto.FromECDSAPub(&k.PublicKey)
 
@@ -30,6 +61,21 @@ func main() {
 			fmt.Println("Address: ", Address(k))
 			fmt.Println("Privatekey: ", PrivateKey(k))
 			break
+		}
+
+		if i%100000 == 0 {
+			br := NewBadRand(i, 40)
+
+			out := make([]byte, 40)
+			n, err := io.ReadFull(br, out)
+			if err != nil {
+				fmt.Println("BAD READ!", err)
+			}
+
+			fmt.Printf("%v bytes read\n", n)
+			fmt.Printf("%v\n", out)
+
+			fmt.Printf("i = %d\n", i)
 		}
 	}
 }
